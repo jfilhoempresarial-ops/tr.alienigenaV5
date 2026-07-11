ce4c44cabbf53d1a511e24b2f56937255a865d17
jfilhoempresarial-ops <jfilhoempresarial@gmail.com>
2026-07-11 20:06:51 -0300

    Refactor navbar to include admin link and actions div

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,7 +3,10 @@ export function renderNavbar() {
   el.innerHTML = `
     <nav class="navbar">
       <a href="#/" class="navbar__logo">TRA da Estrada</a>
-      <a href="#/cadastro-empresa" class="navbar__cta">Cadastrar minha empresa</a>
+      <div class="navbar__acoes">
+        <a href="#/cadastro-empresa" class="navbar__cta">Cadastrar minha empresa</a>
+        <a href="#/admin" class="navbar__admin" title="Área do admin">⚙️</a>
+      </div>
     </nav>
   `;
 }
