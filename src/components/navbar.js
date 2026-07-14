4883b5d6a4e285e8285439573de80146ba9aa006
jfilhoempresarial-ops <jfilhoempresarial@gmail.com>
2026-07-14 09:05:03 -0300

    logo no navbar e estilo do total de vagas

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ export function renderNavbar() {
   const el = document.getElementById('navbar');
   el.innerHTML = `
     <nav class="navbar">
-      <a href="#/" class="navbar__logo">TRA da Estrada</a>
+      <a href="#/" class="navbar__logo"><img src="/images/logo/Logo-tra.png" alt="TRA da Estrada" class="navbar__logo-img"></a>
       <div class="navbar__acoes">
         <a href="#/cadastro-empresa" class="navbar__cta">Cadastrar minha empresa</a>
         <a href="#/admin" class="navbar__admin" title="Área do admin">⚙️</a>
