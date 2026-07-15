a92816a1d6d21c72548a43d0fbfc70531f175cbc
jfilhoempresarial-ops <jfilhoempresarial@gmail.com>
2026-07-14 22:09:16 -0300

    Update navbar links to use absolute paths

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,10 +2,10 @@ export function renderNavbar() {
   const el = document.getElementById('navbar');
   el.innerHTML = `
     <nav class="navbar">
-      <a href="#/" class="navbar__logo"><img src="/images/logo/Logo-tra.png" alt="TRA da Estrada" class="navbar__logo-img"></a>
+      <a href="/" class="navbar__logo"><img src="/images/logo/Logo-tra.png" alt="TRA da Estrada" class="navbar__logo-img"></a>
       <div class="navbar__acoes">
-        <a href="#/cadastro-empresa" class="navbar__cta">Cadastrar minha empresa</a>
-        <a href="#/admin" class="navbar__admin" title="Área do admin">⚙️</a>
+        <a href="/cadastro-empresa" class="navbar__cta">Cadastrar minha empresa</a>
+        <a href="/admin" class="navbar__admin" title="Área do admin">⚙️</a>
       </div>
     </nav>
   `;
